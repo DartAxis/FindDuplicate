0d493e52f2a92e7b449e684a352aa629fe165a90
Alex <filatyev.a.a@gmail.com>
2020-06-08 12:10:30 +0300

    some fixes

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -10,13 +10,14 @@ import java.util.List;
 import java.util.Map;
 
 public class MainProgram {
-    final static int BUFF=16384;
+    final static int BUFF = 16384;
+
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InterruptedException {
         Map<String, List<String>> fileDuplicate = new HashMap<>();
-        fileDuplicate=getListFiles("c:\\dir\\",fileDuplicate);
+        fileDuplicate = getListFiles("c:\\dir\\", fileDuplicate);
 
         fileDuplicate.forEach((a, b) -> {
-            if(b.size()>1) {
+            if (b.size() > 1) {
                 System.out.println("Файлы дубликаты:");
                 b.forEach(x -> {
                     System.out.println(x);
@@ -25,19 +26,19 @@ public class MainProgram {
         });
     }
 
-    private static Map<String, List<String>> getListFiles(String directory, final Map<String, List<String>> fileDuplicate ) throws IOException, NoSuchAlgorithmException {
+    private static Map<String, List<String>> getListFiles(String directory, final Map<String, List<String>> fileDuplicate) throws IOException, NoSuchAlgorithmException {
         final MessageDigest md = MessageDigest.getInstance("SHA-256");//SHA, MD2, MD5, SHA-256, SHA-384
         Files.walk(Paths.get(directory))
                 .filter(Files::isRegularFile)
-                .peek(x->{
+                .peek(x -> {
                     try {
-                        String hash=checksum(x.toString(), md);
-                        if(fileDuplicate.containsKey(hash)){
+                        String hash = checksum(x.toString(), md);
+                        if (fileDuplicate.containsKey(hash)) {
                             fileDuplicate.get(hash).add(x.toString());
                         } else {
-                            List<String> filelist=new ArrayList<>();
+                            List<String> filelist = new ArrayList<>();
                             filelist.add(x.toString());
-                            fileDuplicate.put(hash,filelist);
+                            fileDuplicate.put(hash, filelist);
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -54,7 +55,7 @@ public class MainProgram {
         byte[] buffer = new byte[buff];
         long read = 0;
         long offset = file.length();
-        System.out.print("Processing file "+filepath);
+        System.out.print("Processing file " + filepath);
         int unitsize;
         while (read < offset) {
             unitsize = (int) (((offset - read) >= buff) ? buff : (offset - read));
@@ -68,20 +69,8 @@ public class MainProgram {
             result.append(String.format("%02x", b));
         }
         long timeStop = System.currentTimeMillis();
-        System.out.println(" . Complete in " + (timeStop - timeStart) / 1000 + " sec. With filesize is - " + (fileSize) + " bytes. Hash: "+result.toString());
+        System.out.println(" . Complete in " + (timeStop - timeStart) / 1000 + " sec. With filesize is - " + (fileSize) + " bytes. Hash: " + result.toString());
         return result.toString();
 
     }
-
-   /* private static void compareFiles(Map<String, String> filemap, Map<String, List<String>> fileDuplicate) {
-        for (Map.Entry<String, String> en : filemap.entrySet()) {
-            if(fileDuplicate.containsKey(en.getValue())){
-                fileDuplicate.get(en.getValue()).add(en.getKey());
-            } else {
-                List<String> filelist=new ArrayList<>();
-                filelist.add(en.getKey());
-                fileDuplicate.put(en.getValue(),filelist);
-            }
-        }
-    }*/
 }
